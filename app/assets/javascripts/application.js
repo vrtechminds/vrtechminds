2be792a9df1649b30c06e10b4454737811346be9
Ratnakar <kezy485@gmail.com>
2017-02-01 00:02:34 +0530

    scroll effect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,16 +20,14 @@
 //= require SmoothScroll
 //= require turbolinks
 //= require_tree .
-function main() {
+//= require_self 
 
-(function () {
-   'use strict';
-
+$("document").ready(function(){
    /* ==============================================
-  	Testimonial Slider
-  	=============================================== */ 
+    Testimonial Slider
+    =============================================== */ 
 
-  	$('a.page-scroll').click(function() {
+    $('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -59,42 +57,42 @@ function main() {
         offset: 80
     })
 
-  	$(document).ready(function() {
-  	  $("#team").owlCarousel({
-  	 
-  	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
-  	      paginationSpeed : 400,
-  	      autoHeight : true,
-  	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 4],
-				        [1400, 4],
-				        [1600, 4]
-				      ],
-  	  });
+    $(document).ready(function() {
+      $("#team").owlCarousel({
+     
+          navigation : false, // Show next and prev buttons
+          slideSpeed : 300,
+          paginationSpeed : 400,
+          autoHeight : true,
+          itemsCustom : [
+                [0, 1],
+                [450, 2],
+                [600, 2],
+                [700, 2],
+                [1000, 4],
+                [1200, 4],
+                [1400, 4],
+                [1600, 4]
+              ],
+      });
 
-  	  $("#clients").owlCarousel({
-  	 
-  	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
-  	      paginationSpeed : 400,
-  	      autoHeight : true,
-  	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 5],
-				        [1400, 5],
-				        [1600, 5]
-				      ],
-  	  });
+      $("#clients").owlCarousel({
+     
+          navigation : false, // Show next and prev buttons
+          slideSpeed : 300,
+          paginationSpeed : 400,
+          autoHeight : true,
+          itemsCustom : [
+                [0, 1],
+                [450, 2],
+                [600, 2],
+                [700, 2],
+                [1000, 4],
+                [1200, 5],
+                [1400, 5],
+                [1600, 5]
+              ],
+      });
 
       $("#testimonial").owlCarousel({
         navigation : false, // Show next and prev buttons
@@ -103,9 +101,9 @@ function main() {
         singleItem:true
         });
 
-  	});
+    });
 
-  	/*====================================
+    /*====================================
     Portfolio Isotope Filter
     ======================================*/
     $(window).load(function() {
@@ -134,11 +132,8 @@ function main() {
         });
 
     });
+  
+})
 
 
 
-}());
-
-
-}
-main();
